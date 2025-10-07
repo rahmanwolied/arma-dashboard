1ef4999aa11697766f7e1838420c6347655cfb2b
rahmanwolied <mosheur.r.wolied@gmail.com>
2025-10-07 22:20:12 +0600

    Update Next.js configuration: Added TypeScript build error ignoring, improved formatting for remote image patterns, and ensured consistent use of quotes.

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,24 @@
-import type { NextConfig } from 'next';
-import { withSentryConfig } from '@sentry/nextjs';
+import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs";
 
 // Define the base Next.js configuration
 const baseConfig: NextConfig = {
   eslint: {
-    ignoreDuringBuilds: true
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'api.slingacademy.com',
-        port: ''
-      }
-    ]
+        protocol: "https",
+        hostname: "api.slingacademy.com",
+        port: "",
+      },
+    ],
   },
-  transpilePackages: ['geist']
+  transpilePackages: ["geist"],
 };
 
 let configWithPlugins = baseConfig;
@@ -39,20 +42,20 @@ if (!process.env.NEXT_PUBLIC_SENTRY_DISABLED) {
 
     // Upload a larger set of source maps for prettier stack traces (increases build time)
     reactComponentAnnotation: {
-      enabled: true
+      enabled: true,
     },
 
     // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
     // This can increase your server load as well as your hosting bill.
     // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
     // side errors will fail.
-    tunnelRoute: '/monitoring',
+    tunnelRoute: "/monitoring",
 
     // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
 
     // Disable Sentry telemetry
-    telemetry: false
+    telemetry: false,
   });
 }
 
